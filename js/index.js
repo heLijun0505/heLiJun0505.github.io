86d26349a9bf20593d74f3057c19605161e3b96b
heLiJun0505 <1608484029@qq.com>
2020-07-09 21:14:19 +0800

    index finiish

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 $(function () {
+  // jquery.lazyload.js 初始化
+  $('img').lazyload()
+  // wow.js 初始化
   new WOW().init();
   // advantage tab栏切换
   $(".advantage_tab li").on("click mouseenter", function () {
@@ -7,7 +10,7 @@ $(function () {
       "display" : "block"
     }).siblings().css("display", "none")
   })
-  // 滚动监听
+  // header滚动监听
   var carouselHeight,scrollTop
   function scroll() {
     carouselHeight = $('.carousel-body').height()
@@ -24,4 +27,22 @@ $(function () {
   $(window).scroll(function () {
     scroll()
   });
+  // nav
+  $('.nav-trigger').on('click', function () {
+    $('nav').toggleClass('fade-in')
+  })
+  $('.nav_item').on('click', function () {
+    $('.nav_sub').eq($('.nav_item').index($(this))).slideToggle().siblings('.nav_sub').slideUp()
+    $(this).addClass('nav_active').siblings().removeClass('nav_active')
+  })
+  // search
+  function searchToggleClass($element, classTxt) {
+    $element.toggleClass(classTxt)
+  }
+  $('.search-icon').on('click', function () {
+    searchToggleClass($('.search_warp'), 'search_in')
+  })
+  $('.search_closed').on('click', function () {
+    searchToggleClass($('.search_warp'), 'search_in')
+  })
 })
